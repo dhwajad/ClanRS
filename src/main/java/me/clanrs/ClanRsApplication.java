4462d19d90c9c443cc1e8bea4f939896e47bb943
dhwajad <dhwajad@gmail.com>
2017-12-17 10:56:08 -0500

    Initial commit

---
+++ src/main/java/me/clanrs/ClanRsApplication.java
@@ -0,0 +1,12 @@
+package me.clanrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClanRsApplication.class, args);
+	}
+}
